1575f1ef0e57649b7e37d948b2d14d51d218aaf1
arwaalmuafi <arwaalmaufi@gmail.com>
2024-10-29 11:08:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+        //1
+//        System.out.println("please enter first number: ");
+//        int first = input.nextInt();
+//        System.out.println("please enter second number: ");
+//        int second = input.nextInt();
+//        System.out.println("please enter third number: ");
+//        int third = input.nextInt();
+//
+//        if (first > second && first > third){
+//            System.out.println("thr largest number is: "+first);
+//        }else if (second > first && second > third){
+//            System.out.println("the largest number is: "+second);
+//        }else System.out.println("the largest number is: "+third);
+
+        //2
+
+//        System.out.println("please enter your a String: ");
+//        String word = input.nextLine();
+//        System.out.println("please enter your index: ");
+//        int index = input.nextInt();
+//
+//        char  character =word.charAt(index);
+//        System.out.println("the  character is "+character);
+
+
+        //3
+//        int sum=0;
+//            System.out.println("please enter a count or -1 to quit ");
+//             int count =input.nextInt();
+//             if (count>0) {
+//                 for (count = 1; count > 0; count++) {
+//                     sum = sum + count;
+//                 }
+//                 System.out.println(sum);
+//             }else System.out.println();
+
+
+        //4
+
+//        int [] array = {10, -21 , 30, 31, -25};
+//
+//        for (int n : array){
+//
+//            if (n>0){
+//                System.out.println(n+" is a positive number ");
+//            }else if (n<0) {
+//                System.out.println(n+" is a negative number");
+//            }
+//        }
+
+        //5
+
+//        String [] array = {"Tuwaiq","Bootcamp","Student","JAVA"};
+//        int index=4;
+//         for (String n : array){
+//           if (n.length()==index) {
+//               System.out.println(n);
+//           }
+//         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
